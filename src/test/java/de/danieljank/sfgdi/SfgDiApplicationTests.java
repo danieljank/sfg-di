3467cd0f05590918a5c16661366bde9da5bf7bf9
Daniel Jankowski <daniel.jank@gmail.com>
2020-10-28 23:22:23 +0100

    Initial stuff

---
+++ src/test/java/de/danieljank/sfgdi/SfgDiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.danieljank.sfgdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
